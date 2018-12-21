f4dce15b54c2c2bdeaf00f15ced4d423a318c7f2
chitraphp <chitra.atmakuru@gmail.com>
2018-12-20 23:19:12 -0800

    Add project files.

---
+++ MusicLibraryApp/Model/PlayList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibraryApp.Model
+{
+    public class PlayList
+    {
+        public string PlayListName { get; set; }
+        public ObservableCollection<Song> PlayListSongs { get; set; }
+        public string PlayListFilePath { get; set; }
+        
+        public PlayList(String name)
+        {
+            this.PlayListName = name;
+            this.PlayListSongs = new ObservableCollection<Song>();
+        }
+        
+        public ObservableCollection<Song> GetSongsListOfPlayList()
+        {
+            return this.PlayListSongs;
+        }
+    }
+
+
+}
